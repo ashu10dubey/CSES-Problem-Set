178fedc0303d10dc3ec31e9c0b1d1a4caa9db112
Ashutosh Dubey <ashutoshdubey9794@gmail.com>
2025-06-29 23:06:35 +0530

    Update Reading Books.cpp

--- Sorting and Searching/Reading Books.cpp
+++ Sorting and Searching/Reading Books.cpp
@@ -1,21 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
     int n;
     cin >> n;
-    long long sum = 0, max_t = 0;
+
+    vector<long long> t(n);
+    long long total = 0, max_time = 0;
+
     for (int i = 0; i < n; ++i) {
-        long long x;
-        cin >> x;
-        sum += x;
-        max_t = max(max_t, x);
+        cin >> t[i];
+        total += t[i];
+        max_time = max(max_time, t[i]);
     }
-    cout << max(sum, 2 * max_t - sum) << "\n";
-    // Or simply:
-    // cout << max(sum, max_t) << "\n";
+
+    cout << max(total, 2 * max_time) << '\n';
     return 0;
 }
